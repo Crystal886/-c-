7970368b5dde321ac80b824893dd97bb87f5dc98
Crystal886 <1638134730@qq.com>
2024-08-02 23:43:32 +0800

    c学习
    c学习

--- test_8_1b/test_8_2.c
+++ test_8_1b/test_8_2.c
@@ -30,14 +30,64 @@
 //int main()
 //{
 //	int input = 0;
-//	printf("今天你玩原神了吗？");
+//	printf("？");
 //	scanf("%d", &input);
 //	if (input == 1)
 //		printf("未来可期");
 //	else
-//		print("这辈子没了");
+//		print(".");
 //
 //	return 0;
 //}
-
-
+//int main()
+//{
+//	int line = 0;
+//	while (line < 30000)
+//	{
+//		printf("写代码：%d\n", line);
+//		line++;
+//	}
+//	if (line == 30000)
+//	{
+//		printf("好offer\n");
+//	}
+//	return 0;
+//}
+//
+// 
+//
+//Add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	scanf("%d%d", &num1, &num2);
+//	//int sum = num1 + num2;
+//	int sum=Add(num1, num2);
+//	printf("%d\n", sum);
+//	return 0;
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int i = 0;
+//	while (i < 10)
+//	{
+//
+//		printf("%d ",  arr[i]);
+//		i++;
+//	}
+//
+//	return 0;
+//}
+int main()
+{
+	int a = 2;
+	int b = a << 1;
+	printf("%d\n", b);
+	return 0;
+}
