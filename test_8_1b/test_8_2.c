223ae17c6fedddba737f021e2b1926bddf2e4ba9
Crystal886 <1638134730@qq.com>
2024-08-02 19:42:20 +0800

    学习
    学习

---
+++ test_8_1b/test_8_2.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//#define MAX 10000
+//int main()
+
+	/*int a = 10;
+	printf("a=%d\n", a);*/
+	//const int num = 10;//const常变量
+	//const int n;
+	//int arr[n] = { 0 };//const修饰的常变量
+	//具有常属性，但本质上还是变量
+	//define定义的标识符常量int n = MAX;
+	//printf("%d\n", n);
+	//char arr[] = "hello";
+	/*char arr1[] = "abc";
+	char arr2[] = { 'a','b','c','\0'};
+	printf("%s\n",arr1);
+	printf("%s\n",arr2)*/
+	/*int len = strlen("abc");
+	printf(" % d\n", len);
+
+
+	return 0;
+}*/
+//int main()
+//{
+//	printf("\a \a \a");//%c打印单字符用‘’ %s打印字符串用“”
+//	return 0;
+//}
+//int main()
+//{
+//	int input = 0;
+//	printf("今天你玩原神了吗？");
+//	scanf("%d", &input);
+//	if (input == 1)
+//		printf("未来可期");
+//	else
+//		print("这辈子没了");
+//
+//	return 0;
+//}
+
+
