8c8a405559acdaea7aa2828c3f6b290f83dfff03
Crystal886 <1638134730@qq.com>
2024-09-04 22:35:55 +0800

    9.4
    练习

---
+++ test_9_4_2024/test_9_4.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int sum(int a)
+//{
+//	static int b = 3;
+//	int c = 5;
+//	b += 2;
+//	c += 5;
+//	return (a + b + c);
+//}
+//int main()
+//{
+//
+//	int i;
+//	int a = 2;
+//	for (i = 0; i < 5; i++)
+//	{
+//
+//		printf("%d,", sum(a));
+//	}
+//	return 0;
+//}
+int main()
+{
+	printf("     **\n");
+	printf("     **\n");
+	printf("************\n");
+
+	return 0;
+}
